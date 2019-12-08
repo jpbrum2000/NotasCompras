5656a3cb32498d9e813f940fa8f0afc749b37288
joao <jpbrum2000@gmail.com>
2019-12-07 21:58:34 -0300

    Criado Tabela de Usuario e Populado Inicialmente

---
+++ src/Repository/Migrations/20191208000738_CreateUsuarioTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class CreateUsuarioTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Login = table.Column<string>(nullable: true),
+                    Senha = table.Column<string>(nullable: true),
+                    ValorMinimo = table.Column<double>(nullable: false),
+                    ValorMaximo = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
---
+++ src/Repository/Migrations/20191208003257_SeedUsuarioTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class SeedUsuarioTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Papel",
+                table: "Usuario",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.InsertData(
+                table: "Usuario",
+                columns: new[] { "Id", "Login", "Papel", "Senha", "ValorMaximo", "ValorMinimo" },
+                values: new object[] { 1, "gerente", 1, "gerente", 999999.98999999999, 50000.010000000002 });
+
+            migrationBuilder.InsertData(
+                table: "Usuario",
+                columns: new[] { "Id", "Login", "Papel", "Senha", "ValorMaximo", "ValorMinimo" },
+                values: new object[] { 2, "subgerente", 1, "subgerente", 999999.98999999999, 1000.01 });
+
+            migrationBuilder.InsertData(
+                table: "Usuario",
+                columns: new[] { "Id", "Login", "Papel", "Senha", "ValorMaximo", "ValorMinimo" },
+                values: new object[] { 3, "vendedor", 0, "vendedor", 999999.98999999999, 10000.01 });
+
+            migrationBuilder.InsertData(
+                table: "Usuario",
+                columns: new[] { "Id", "Login", "Papel", "Senha", "ValorMaximo", "ValorMinimo" },
+                values: new object[] { 4, "auxiliar", 0, "auxiliar", 999999.98999999999, 0.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Usuario",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Usuario",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Usuario",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Usuario",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DropColumn(
+                name: "Papel",
+                table: "Usuario");
+        }
+    }
+}
