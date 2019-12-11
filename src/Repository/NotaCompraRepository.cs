c251b1481bb4c3334c8163c88d0c155a51233e43
joao <jpbrum2000@gmail.com>
2019-12-11 16:40:48 -0300

    Organizado Regras de Negocio

--- src/Repository/NotaCompraRepository.cs
+++ src/Repository/NotaCompraRepository.cs
@@ -17,12 +17,18 @@ namespace Repository {
             Usuario usuario = _context.Usuario.Find(usuarioId);
 
             IQueryable<NotaCompra> query = _context.NotasCompra.Include(n => n.HistAprovNotasCompra)
+            .Where(nf => nf.Status != Status.Aprovada)
             .Where(nf => dataInicio <= nf.DataEmissao && nf.DataEmissao <= dataFim)
             .Where(nf => usuario.ValorMinimo <= nf.ValorTotal && nf.ValorTotal <= usuario.ValorMaximo)
             .Where(nf => nf.HistAprovNotasCompra.Where(h => h.UsuarioId == usuario.Id).Count() == 0);
             NotaCompra[] NFresult = await query.ToArrayAsync();
-            
-            return NFresult.Where(nf => usuario.Papel == Papel.Visto ? nf.PrecisaVisto(GetConfNumVistoByValorNF(nf.ValorTotal).Result) : !nf.PrecisaVisto(GetConfNumVistoByValorNF(nf.ValorTotal).Result) && nf.PrecisaAprovacao(GetConfNumAprovacoesByValorNF(nf.ValorTotal).Result)).ToArray();
+
+            if (usuario.Papel == Papel.Visto ) {
+                return NFresult.Where(nf => nf.PrecisaVisto(GetConfNumVistoByValorNF(nf.ValorTotal).Result)).ToArray();
+            } 
+            else {
+                return NFresult.Where(nf => nf.PrecisaAprovacao(GetConfNumVistoByValorNF(nf.ValorTotal).Result, GetConfNumAprovacoesByValorNF(nf.ValorTotal).Result)).ToArray();
+            }
         }
 
         public async Task<bool> RegistraVistoAprovacaoAsyncById(int idNotaCompra, int usuarioId)
@@ -30,37 +36,22 @@ namespace Repository {
             Usuario usuario = _context.Usuario.Find(usuarioId);
             NotaCompra nf = await _context.NotasCompra.Include(n => n.HistAprovNotasCompra).SingleAsync(n => n.Id == idNotaCompra);
             ConfiguracaoFaixaVistosAprovacoes ConfFaixaVistAprov = await _context.ConfFaixaVistAprov.Where(conf => conf.FaixaMin <= nf.ValorTotal && nf.ValorTotal <= conf.FaixaMax).SingleAsync();
-                    
-            if ( nf.HistAprovNotasCompra.Where(h => h.Id == idNotaCompra && h.Usuario == usuario ).Count() == 0 && nf.Status != Status.Aprovada) {
-                int numVistos = nf.HistAprovNotasCompra.Where(h => h.Operacao == Operacao.Visto).Count();
-                int numAprovacoes = nf.HistAprovNotasCompra.Where(h => h.Operacao == Operacao.Aprovacao).Count();
-                
-                if (usuario.Papel == Papel.Visto && ConfFaixaVistAprov.Vistos > numVistos) {
-                    _context.Add(new HistoricoAprovacaoNotaCompra {
-                            Data = DateTime.Now,
-                            UsuarioId = usuario.Id,
-                            Operacao = Operacao.Visto,
-                            NotaCompraId = nf.Id
-                    });
-                    await _context.SaveChangesAsync();
-                }
-                else if (usuario.Papel == Papel.Aprovacao && ConfFaixaVistAprov.Vistos == numVistos) {
-                    
-                    if (numVistos >= ConfFaixaVistAprov.Vistos) {
-                        _context.Add(new HistoricoAprovacaoNotaCompra{
-                            Data = DateTime.Now,
-                            UsuarioId = usuario.Id,
-                            Operacao = Operacao.Aprovacao,
-                            NotaCompraId = nf.Id
-                        });
-                        await _context.SaveChangesAsync();
-                    }
-                }
 
-                // Aprovacao da Nota
-                numVistos = nf.HistAprovNotasCompra.Where(h => h.Operacao == Operacao.Visto).Count();
-                numAprovacoes = nf.HistAprovNotasCompra.Where(h => h.Operacao == Operacao.Aprovacao).Count();
-                if (ConfFaixaVistAprov.Vistos == numVistos && ConfFaixaVistAprov.Aprovacoes == numAprovacoes){
+            //Verifica se Usuario já nao aprovou/Vistoriou Nota
+            bool usuarioJaRegistrou = (nf.HistAprovNotasCompra.Where(h => h.Id == idNotaCompra && h.Usuario == usuario ).Count() != 0);
+            bool podeRegistrarNF = (usuario.Papel == Papel.Visto && nf.PrecisaVisto(ConfFaixaVistAprov.Vistos)) || (usuario.Papel == Papel.Aprovacao && nf.PrecisaAprovacao(ConfFaixaVistAprov.Vistos, ConfFaixaVistAprov.Aprovacoes));
+            //Registra VistoAprov
+            if (!usuarioJaRegistrou && podeRegistrarNF) {
+                HistoricoAprovacaoNotaCompra HistAprovNF = new HistoricoAprovacaoNotaCompra {
+                    Data = DateTime.Now,
+                    UsuarioId = usuario.Id,
+                    Operacao = usuario.Papel == Papel.Visto ? Operacao.Visto : Operacao.Aprovacao,
+                    NotaCompraId = nf.Id
+                };
+                _context.Add(HistAprovNF);
+                await _context.SaveChangesAsync();
+                // Aprova da Nota
+                if (nf.PodeAprovar(ConfFaixaVistAprov.Vistos,ConfFaixaVistAprov.Aprovacoes)){
                     nf.Status = Status.Aprovada;
                     _context.Update(nf);
                     return await _context.SaveChangesAsync() > 0;
